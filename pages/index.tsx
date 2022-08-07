da160e042ee91db2a8617dc2a728a3b28c4aaf82
Daniel Le'Sage <DLesage25@users.noreply.github.com>
2022-08-07 15:07:24 -0600

    Feat: pokemon loader infinite scroll (#7)
    * feat: infinite scroll
    
    * chore: add loader component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,13 @@
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
-
-import { PokemonId } from '../types/pokemon.type';
 import Layout from '../components/molecules/Layout';
 import PokemonProfiler from '../components/organisms/PokemonProfiler';
-import useStore from '../state/store';
-import pokeApi from '../utils/pokeApiHttp';
 
-interface HomePageProps {
-    pokeIds: PokemonId[];
-}
-
-const Home: NextPage<HomePageProps> = ({ pokeIds }: HomePageProps) => {
-    const { pokemonIds, setPokemonIds } = useStore();
-
-    useEffect(() => {
-        setPokemonIds(pokeIds);
-    }, [pokeIds, setPokemonIds]);
-
-    return <Layout>{pokemonIds.length && <PokemonProfiler />}</Layout>;
+const Home: NextPage = () => {
+    return (
+        <Layout>
+            <PokemonProfiler />
+        </Layout>
+    );
 };
-
-export async function getStaticProps() {
-    const { data } = await pokeApi.get('/', {
-        params: {
-            limit: 15,
-        },
-    });
-
-    return {
-        props: {
-            pokeIds: data.results,
-        },
-    };
-}
 
 export default Home;
