58658afb702a546c650a101321d4f68aa6217353
Daniel Le'Sage <DLesage25@users.noreply.github.com>
2022-08-07 16:00:29 -0600

    feat: adds working search component (#8)

--- state/store.ts
+++ state/store.ts
@@ -1,24 +1,18 @@
-import produce from 'immer';
 import create from 'zustand';
-import { Pokemon, PokemonId } from '../types/pokemon.type';
+import { PokemonId } from '../types/pokemon.type';
 
 interface InitialState {
     pokemonIds: Array<PokemonId>;
     setPokemonIds: (pokemonIds: Array<PokemonId>) => void;
-    pokemons: Array<Pokemon>;
-    setPokemons: (pokemon: Pokemon) => void;
+    searchQuery: string;
+    setSearchQuery: (query: string) => void;
 }
 
 const useStore = create<InitialState>()((set) => ({
     pokemonIds: [],
     setPokemonIds: (data) => set(() => ({ pokemonIds: data })),
-    pokemons: [],
-    setPokemons: (data) =>
-        set(
-            produce((state: InitialState) => {
-                state.pokemons.push(data);
-            })
-        ),
+    searchQuery: '',
+    setSearchQuery: (query) => set(() => ({ searchQuery: query })),
 }));
 
 export default useStore;
