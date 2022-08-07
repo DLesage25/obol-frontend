0013fa0bd15ca113cedb9202b69737113601053f
Daniel Le'Sage <daniel@partnerhero.com>
2022-08-07 15:06:13 -0600

    feat: infinite scroll

--- components/organisms/PokemonProfiler/PokemonProfiler.tsx
+++ components/organisms/PokemonProfiler/PokemonProfiler.tsx
@@ -1,58 +1,25 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+import isBottom from '../../../hooks/isBottom';
 
 import useStore from '../../../state/store';
-import { styled } from '../../../styles/theme';
 import { PokemonId } from '../../../types/pokemon.type';
+import debounce from '../../../utils/debounce';
 import partitionArray from '../../../utils/partitionArray';
-import PokemonCard from '../../molecules/PokemonCard';
-
-const OuterContainer = styled('div', {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: '24px',
-});
-
-const InnerContainer = styled('div', {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-    gap: '32px',
-    padding: '0px, 80px',
-});
-
-const fetchPokemonProfile = async (url: string) => {
-    const res = await fetch(url);
-    return res.json();
-};
-
-const PokemonCardLoader = ({ pokemonId }: { pokemonId: PokemonId }) => {
-    const { url, name } = pokemonId;
-
-    const { data } = useQuery(['pokemonProfile', url], () =>
-        fetchPokemonProfile(url)
-    );
-
-    const link = `https://bulbapedia.bulbagarden.net/wiki/${name}`;
-    const imageSrc = `https://img.pokemondb.net/artwork/large/${name}.jpg`;
-    return (
-        <PokemonCard
-            key={Math.random()}
-            title={name}
-            imageSrc={imageSrc}
-            profile={data}
-            link={link}
-        />
-    );
-};
+import { fetchPokemon } from '../../../utils/pokeApiHttp';
+import Loader from '../../atoms/Loader/Loader';
+import {
+    InnerContainer,
+    OuterContainer,
+} from '../../atoms/PokemonProfiler/Containers';
+import PokemonLoader from '../../molecules/PokemonLoader/PokemonLoader';
 
 const ProfilerRow = ({ data }: { data: PokemonId[] }) => {
     return (
         <InnerContainer>
             {data.map((pokemonId) => {
                 return (
-                    <PokemonCardLoader
+                    <PokemonLoader
                         key={`pokemoncardloader-${pokemonId.name}`}
                         pokemonId={pokemonId}
                     />
@@ -63,16 +30,45 @@ const ProfilerRow = ({ data }: { data: PokemonId[] }) => {
 };
 
 export default function PokemonProfiler() {
-    const { pokemonIds } = useStore();
-
+    const { pokemonIds, setPokemonIds } = useStore();
     const sortedData = partitionArray(pokemonIds, 3);
+    const observer = useRef<HTMLDivElement>(null);
+
+    const { data, isLoading, fetchNextPage, isFetchingNextPage } =
+        useInfiniteQuery(['pokemon'], fetchPokemon, {
+            getNextPageParam: (lastPage) => lastPage.nextPage,
+        });
+
+    useEffect(() => {
+        if (!data) return;
+
+        const pokeIds = data?.pages
+            .map((group, i) =>
+                group.response.map((pokemonIds: PokemonId[]) => pokemonIds)
+            )
+            .flat();
+        setPokemonIds(pokeIds);
+    }, [data, setPokemonIds]);
+
+    useEffect(() => {
+        let debouncedFetch = debounce(fetchNextPage, 50);
+
+        const onScroll = () => {
+            if (!isLoading && isBottom(observer)) {
+                debouncedFetch();
+            }
+        };
+        document.addEventListener('scroll', onScroll);
+        return () => document.removeEventListener('scroll', onScroll);
+    }, [isLoading, fetchNextPage]);
 
     return (
-        <OuterContainer>
+        <OuterContainer ref={observer}>
             {sortedData &&
                 sortedData.map((chunk: any) => {
                     return <ProfilerRow key={Math.random()} data={chunk} />;
                 })}
+            {isFetchingNextPage && <Loader />}
         </OuterContainer>
     );
 }
